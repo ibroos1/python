5f895209e5b3ff2515eeab1dcc6e7e84405646b6
ibroos1 <ibrahimadul20@gmail.com>
2023-03-31 16:06:13 +0300

    pyhton tasks 4

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 number1 = int(input("enter number : "))
-if number1/2==1 : 
+if number1%2==0 : 
     print("even number ")
 else :
     print("odd number")
---
+++ task4.py
@@ -0,0 +1,5 @@
+email = input("enter your email");
+if "@" in email and "." in email : 
+    print(email)
+else :
+    print("invalid email")
